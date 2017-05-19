bc828c11bc1370aaa5993f74c762fb4fd484de23
JPisaBrony <jdpohl789@gmail.com>
2017-05-18 19:57:06 -0500

    + selecting colors and setting them

--- client/main.js
+++ client/main.js
@@ -1,18 +1,24 @@
 import { Template } from 'meteor/templating';
 import './main.html';
 
-var selectedImage = new Image(16, 16);
+// GLOBALS
+var textureSize = 16, i, j;
+var selectedImage = new Image(textureSize, textureSize);
 var canvas = document.createElement('canvas');
-canvas.width = 16;
-canvas.height = 16;
-var canCtx = canvas.getContext('2d');
-canCtx.rect(0, 0, 16, 16);
-canCtx.fileSytle = "#FF0000";
-canCtx.fill();
-selectedImage.src = canvas.toDataURL();
+canvas.width = textureSize;
+canvas.height = textureSize;
+var selCtx = canvas.getContext('2d');
+
+function createImageFromColor(color) {
+    var img = new Image(textureSize, textureSize);
+    selCtx.rect(0, 0, textureSize, textureSize);
+    selCtx.fillStyle = color;
+    selCtx.fill();
+    img.src = canvas.toDataURL();
+    return img;
+}
 
 Template.main.rendered = function() {
-    var textureSize = 16, i, j;
     var c = document.getElementById("mainCanvas");
     var w = window.innerWidth;
     var h = window.innerHeight;
@@ -22,10 +28,7 @@ Template.main.rendered = function() {
     c.height = h;
     var ctx = c.getContext("2d");
     
-    var img = new Image();
-    img.src = "sky-01.png";
-    var img2 = new Image();
-    img2.src = "stone-01.png";
+    var img = createImageFromColor("#00ECFF");
 
     for(i = 0; i < w+textureSize; i += textureSize) {
         grid[i/textureSize] = [];
@@ -41,23 +44,13 @@ Template.main.rendered = function() {
             }
         }
     }
-
-    img.onload = function() {
-    img2.onload = function() {
-    for(i = 0; i < w; i += textureSize) {
-        for(j = 0; j < h; j += textureSize) {
-            ctx.drawImage(grid[Math.floor(i/textureSize)][Math.floor(j/textureSize)], i, j);
-        }
-    }
     
     $(document).mousemove(function(e) {
         var x = Math.floor(e.pageX / textureSize) * textureSize;
         var y = Math.floor(e.pageY / textureSize) * textureSize;
         if(lastSel.x != x || lastSel.y != y) {
-            //ctx.clearRect(lastSel.x, lastSel.y, textureSize, textureSize);
             if(lastSel.x != -1 && lastSel.y != -1)
                 ctx.drawImage(grid[Math.floor(lastSel.x/textureSize)][Math.floor(lastSel.y/textureSize)], lastSel.x, lastSel.y);
-                //redrawSection(Math.floor(lastSel.x/textureSize) - 2, Math.floor(lastSel.y/textureSize) - 2, 3, 3, lastSel.x - 1, lastSel.y - 1);
             ctx.beginPath();
             ctx.rect(x + 1, y + 1, textureSize - 2, textureSize - 2);
             ctx.closePath();
@@ -77,7 +70,14 @@ Template.main.rendered = function() {
         ctx.closePath();
         ctx.stroke();
     });
-    }
+    
+    // initial render of all tiles
+    img.onload = function() {
+        for(i = 0; i < w; i += textureSize) {
+            for(j = 0; j < h; j += textureSize) {
+                ctx.drawImage(grid[Math.floor(i/textureSize)][Math.floor(j/textureSize)], i, j);
+            }
+        }
     }
 }
 
@@ -99,7 +99,9 @@ Template.main.events({
         }
     },
     'click li': function() {
-        selectedImage.src = this.color;
-        //console.log(this.color);
+        selCtx.rect(0, 0, textureSize, textureSize);
+        selCtx.fillStyle = this.color;
+        selCtx.fill();
+        selectedImage.src = canvas.toDataURL();
     }
 });
